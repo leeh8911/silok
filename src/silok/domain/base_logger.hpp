e1f762fb6f0cd48e4e39676cdd97f1f06eae6591
sangwon lee <leeh8911@gmail.com>
2025-06-28 22:52:41 +0900

    Refactor logging macros to simplify usage and update logger include path

--- src/silok/domain/base_logger.hpp
+++ src/silok/domain/base_logger.hpp
@@ -48,12 +48,11 @@ class Logger
 
 }  // namespace silok::domain
 
-#define SILOK_LOG(level, fmt, ...) \
-    silok::domain::Logger::GetLogger().level(std::format(fmt, __VA_ARGS__))
+#define SILOK_LOG(level, ...) silok::domain::Logger::GetLogger().level(std::format(__VA_ARGS__))
 
-#define SILOK_LOG_TRACE(fmt, ...) SILOK_LOG(Trace, fmt, __VA_ARGS__)
-#define SILOK_LOG_DEBUG(fmt, ...) SILOK_LOG(Debug, fmt, __VA_ARGS__)
-#define SILOK_LOG_INFO(fmt, ...) SILOK_LOG(Info, fmt, __VA_ARGS__)
-#define SILOK_LOG_WARN(fmt, ...) SILOK_LOG(Warn, fmt, __VA_ARGS__)
-#define SILOK_LOG_ERROR(fmt, ...) SILOK_LOG(Error, fmt, __VA_ARGS__)
-#define SILOK_LOG_CRITICAL(fmt, ...) SILOK_LOG(Critical, fmt, __VA_ARGS__)
+#define SILOK_LOG_TRACE(...) SILOK_LOG(Trace, __VA_ARGS__)
+#define SILOK_LOG_DEBUG(...) SILOK_LOG(Debug, __VA_ARGS__)
+#define SILOK_LOG_INFO(...) SILOK_LOG(Info, __VA_ARGS__)
+#define SILOK_LOG_WARN(...) SILOK_LOG(Warn, __VA_ARGS__)
+#define SILOK_LOG_ERROR(...) SILOK_LOG(Error, __VA_ARGS__)
+#define SILOK_LOG_CRITICAL(...) SILOK_LOG(Critical, __VA_ARGS__)
